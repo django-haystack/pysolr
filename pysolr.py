1b5c427ef919948f3caa74674feca94787527f5f
Daniel Lindsley <daniel@toastdriven.com>
2010-06-19 22:24:45 -0500

    Slicing ``None`` doesn't work. Make it a string...

--- pysolr.py
+++ pysolr.py
@@ -240,10 +240,10 @@ class Solr(object):
             
             try:
                 start_time = time.time()
-                self.log.debug("Starting request to '%s' (%s) with body '%s'..." % (url, method, body[:10]))
+                self.log.debug("Starting request to '%s' (%s) with body '%s'..." % (url, method, str(body)[:10]))
                 headers, response = http.request(url, method=method, body=body, headers=headers)
                 end_time = time.time()
-                self.log.info("Finished '%s' (%s) with body '%s' in %0.3f seconds." % (url, method, body[:10], end_time - start_time))
+                self.log.info("Finished '%s' (%s) with body '%s' in %0.3f seconds." % (url, method, str(body)[:10], end_time - start_time))
             except AttributeError:
                 # For httplib2.
                 error_message = "Failed to connect to server at '%s'. Are you sure '%s' is correct? Checking it in a browser might help..." % (url, self.base_url)
@@ -262,11 +262,11 @@ class Solr(object):
             
             conn = HTTPConnection(self.host, self.port)
             start_time = time.time()
-            self.log.debug("Starting request to '%s:%s/%s' (%s) with body '%s'..." % (self.host, self.port, path, method, body[:10]))
+            self.log.debug("Starting request to '%s:%s/%s' (%s) with body '%s'..." % (self.host, self.port, path, method, str(body)[:10]))
             conn.request(method, path, body, headers)
             response = conn.getresponse()
             end_time = time.time()
-            self.log.info("Finished '%s:%s/%s' (%s) with body '%s' in %0.3f seconds." % (self.host, self.port, path, method, body[:10], end_time - start_time))
+            self.log.info("Finished '%s:%s/%s' (%s) with body '%s' in %0.3f seconds." % (self.host, self.port, path, method, str(body)[:10], end_time - start_time))
             
             if response.status != 200:
                 error_message = self._extract_error(dict(response.getheaders()), response.read())
