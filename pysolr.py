3e796286aed38c618daadeea2a0289b9e1530ac4
David Cramer <dcramer@gmail.com>
2011-05-09 16:48:04 -0700

    Unescape html entities in error messages

--- pysolr.py
+++ pysolr.py
@@ -121,6 +121,7 @@ document 7
 # TODO: unicode support is pretty sloppy. define it better.
 
 from datetime import datetime
+import htmlentitydefs
 import logging
 import re
 import time
@@ -194,6 +195,34 @@ if False:
     LOG.addHandler(stream)
 
 
+def unescape_html(text):
+    """
+    Removes HTML or XML character references and entities from a text string.
+    
+    @param text The HTML (or XML) source text.
+    @return The plain text, as a Unicode string, if necessary.
+
+    Source: http://effbot.org/zone/re-sub.htm#unescape-html
+    """
+    def fixup(m):
+        text = m.group(0)
+        if text[:2] == "&#":
+            # character reference
+            try:
+                if text[:3] == "&#x":
+                    return unichr(int(text[3:-1], 16))
+                else:
+                    return unichr(int(text[2:-1]))
+            except ValueError:
+                pass
+        else:
+            # named entity
+            try:
+                text = unichr(htmlentitydefs.name2codepoint[text[1:-1]])
+            except KeyError:
+                pass
+        return text # leave as is
+    return re.sub("&#?\w+;", fixup, text)
 
 def safe_urlencode(params, doseq=0):
     """
@@ -373,7 +402,7 @@ class Solr(object):
         msg = "[Reason: %s]" % reason
         
         if reason is None:
-            msg += "\n%s" % full_html
+            msg += "\n%s" % unescape_html(full_html)
         
         return msg
     
