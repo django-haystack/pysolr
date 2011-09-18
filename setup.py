f2ffdd7d0de4f8fbbec6b677580a456d6eaa72f7
Daniel Lindsley <daniel@toastdriven.com>
2011-09-18 02:08:59 -0500

    Added a long description for PyPI.

--- setup.py
+++ setup.py
@@ -6,6 +6,7 @@ setup(
     description = "Lightweight python wrapper for Apache Solr.",
     author = 'Daniel Lindsley',
     author_email = 'daniel@toastdriven.com',
+    long_description=open('README', 'r').read(),
     py_modules = ['pysolr'],
     classifiers = [
         'Development Status :: 5 - Production/Stable',
