172b1890bf8440777bd014df1778da24f4015766
Chris Adams <chris@improbable.org>
2012-10-05 13:18:46 -0400

    Catch IOError in _send_request
    httplib2 can raise a bare socket.error in _send_request, which handles only
    AttributeError. This change catches all IOError subclasses, tells logging to
    include exception information and moves logging code outside of the try/except
    block to avoid any possibility of an exception in a log handler being caught by
    mistake.

--- pysolr.py
+++ pysolr.py
@@ -307,19 +307,21 @@ class Solr(object):
             http = Http(timeout=self.timeout)
             url = self.base_url + path
 
+            self.log.debug("Starting request to '%s' (%s) with body '%s'...",
+                           url, method, str(body)[:10])
+            start_time = time.time()
+
             try:
-                start_time = time.time()
-                self.log.debug("Starting request to '%s' (%s) with body '%s'...",
-                               url, method, str(body)[:10])
                 headers, response = http.request(url, method=method, body=body, headers=headers)
-                end_time = time.time()
-                self.log.info("Finished '%s' (%s) with body '%s' in %0.3f seconds.",
-                              url, method, str(body)[:10], end_time - start_time)
-            except AttributeError:
+            except (IOError, AttributeError):
                 error_message = "Failed to connect to server at '%s'. Are you sure '%s' is correct? Checking it in a browser might help..."
                 params = (url, self.base_url)
                 self.log.error(error_message, *params, exc_info=True)
                 raise SolrError(error_message % params)
+
+            end_time = time.time()
+            self.log.info("Finished '%s' (%s) with body '%s' in %0.3f seconds.",
+                          url, method, str(body)[:10], end_time - start_time)
 
             if int(headers['status']) != 200:
                 error_message = self._extract_error(headers, response)
