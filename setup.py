9d8644be93acf3d0e9f9bf99fe417522fcd32c8a
Daniel Lindsley <daniel@toastdriven.com>
2013-01-23 01:12:48 -0600

    Bumped to v3.0.0, adding Python3 support!
    Dependencies have changed & been slimmed down.

--- setup.py
+++ setup.py
@@ -1,23 +1,38 @@
-from distutils.core import setup
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
 
 setup(
-    name = "pysolr",
-    version = "2.1.0",
-    description = "Lightweight python wrapper for Apache Solr.",
-    author = 'Daniel Lindsley',
-    author_email = 'daniel@toastdriven.com',
-    long_description=open('README', 'r').read(),
-    py_modules = ['pysolr'],
-    classifiers = [
+    name="pysolr",
+    version="3.0.0",
+    description="Lightweight python wrapper for Apache Solr.",
+    author='Daniel Lindsley',
+    author_email='daniel@toastdriven.com',
+    long_description=open('README.rst', 'r').read(),
+    py_modules=[
+        'pysolr'
+    ],
+    classifiers=[
         'Development Status :: 5 - Production/Stable',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: BSD License',
         'Operating System :: OS Independent',
         'Programming Language :: Python',
-        'Topic :: Internet :: WWW/HTTP :: Indexing/Search'
+        'Topic :: Internet :: WWW/HTTP :: Indexing/Search',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 3',
     ],
-    url = 'http://github.com/toastdriven/pysolr/',
+    url='http://github.com/toastdriven/pysolr/',
+    license='BSD',
+    install_requires=[
+        'requests>=1.1.0'
+    ],
     extra_requires={
-        'tomcat': ['BeautifulSoup'],
+        'tomcat': [
+            'lxml',
+            'cssselect',
+        ],
     }
 )
