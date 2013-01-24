179ce33d780d974847846c97eafc528c27a56488
Chris Adams <chris@improbable.org>
2013-01-24 11:14:30 -0500

    Update version to 3.0.2

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ except ImportError:
 
 setup(
     name="pysolr",
-    version="3.0.1",
+    version="3.0.2",
     description="Lightweight python wrapper for Apache Solr.",
     author='Daniel Lindsley',
     author_email='daniel@toastdriven.com',
