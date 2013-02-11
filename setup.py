1e117182e6169645940a7d7acc3eba9181e5715e
Chris Adams <chris@improbable.org>
2013-02-11 17:15:47 -0500

    Tag version 3.0.4 for PyPI
    3.x had a minor bug (see SHA:74b0a36) but it broke logging for Solr
    errors which seems worth an easily deployed fix

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ except ImportError:
 
 setup(
     name="pysolr",
-    version="3.0.3",
+    version="3.0.4",
     description="Lightweight python wrapper for Apache Solr.",
     author='Daniel Lindsley',
     author_email='daniel@toastdriven.com',
