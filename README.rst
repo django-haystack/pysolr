ae4083e6111594b7e7d1cd078b80e8e1fdb7aa59
Daniel Lindsley <daniel@toastdriven.com>
2013-07-16 22:32:00 -0700

    Bumped to v3.1.0!

--- README.rst
+++ README.rst
@@ -28,7 +28,7 @@ Features
 Requirements
 ============
 
-* Python 2.6-3.3
+* Python 2.6 - 3.3
 * Requests 1.1.0+
 * **Optional** - ``lxml``
 * **Optional** - ``simplejson``
--- pysolr.py
+++ pysolr.py
@@ -51,7 +51,7 @@ except NameError:
 
 __author__ = 'Daniel Lindsley, Joseph Kocherhans, Jacob Kaplan-Moss'
 __all__ = ['Solr']
-__version__ = (3, 0, 5)
+__version__ = (3, 1, 0)
 
 
 def get_version():
