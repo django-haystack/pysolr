75db499aea1f35853b9061ea4638adb4a86b4f1b
Chris Adams <chris@improbable.org>
2014-02-06 10:46:42 -0500

    Fix type in SolrAdmin.create default parameter
    See #36

--- pysolr.py
+++ pysolr.py
@@ -953,7 +953,7 @@ class SolrCoreAdmin(object):
 
         return self._get_url(self.url, params=params)
 
-    def create(self, name, instance_dir=None, config='solrcofig.xml', schema='schema.xml'):
+    def create(self, name, instance_dir=None, config='solrconfig.xml', schema='schema.xml'):
         """http://wiki.apache.org/solr/CoreAdmin#head-7ca1b98a9df8b8ca0dcfbfc49940ed5ac98c4a08"""
         params = {
             'action': 'STATUS',
