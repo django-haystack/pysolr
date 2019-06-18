b4a011e290b25dbdcc88755d9582b07788595095
Chris Adams <cadams@loc.gov>
2019-06-18 11:07:34 -0400

    Blacken everything

--- setup.py
+++ setup.py
@@ -8,31 +8,23 @@ setup(
     name="pysolr",
     use_scm_version=True,
     description="Lightweight Python client for Apache Solr",
-    author='Daniel Lindsley',
-    author_email='daniel@toastdriven.com',
-    long_description=open('README.rst', 'r').read(),
-    py_modules=[
-        'pysolr'
-    ],
+    author="Daniel Lindsley",
+    author_email="daniel@toastdriven.com",
+    long_description=open("README.rst", "r").read(),
+    py_modules=["pysolr"],
     classifiers=[
-        'Development Status :: 5 - Production/Stable',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: BSD License',
-        'Operating System :: OS Independent',
-        'Programming Language :: Python',
-        'Topic :: Internet :: WWW/HTTP :: Indexing/Search',
-        'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 3',
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: BSD License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Topic :: Internet :: WWW/HTTP :: Indexing/Search",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 3",
     ],
-    url='https://github.com/django-haystack/pysolr/',
-    license='BSD',
-    install_requires=[
-        'requests>=2.9.1'
-    ],
-    extras_require={
-        'solrcloud': [
-            'kazoo>=2.5.0'
-        ]
-    },
-    setup_requires=['setuptools_scm'],
+    url="https://github.com/django-haystack/pysolr/",
+    license="BSD",
+    install_requires=["requests>=2.9.1"],
+    extras_require={"solrcloud": ["kazoo>=2.5.0"]},
+    setup_requires=["setuptools_scm"],
 )
