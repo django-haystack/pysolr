5971ca9808b1e9246d7a274d1278c16b53285e3e
Brian Morton <rokclimb15@gmail.com>
2020-03-22 16:08:45 +0000

    Ignore clusterstate.json. Fixes #224.

--- pysolr.py
+++ pysolr.py
@@ -1482,7 +1482,6 @@ class ZooKeeper(object):
     # Constants used by the REST API:
     LIVE_NODES_ZKNODE = "/live_nodes"
     ALIASES = "/aliases.json"
-    CLUSTER_STATE = "/clusterstate.json"
     COLLECTION_STATUS = "/collections"
     COLLECTION_STATE = "/collections/%s/state.json"
     SHARDS = "shards"
@@ -1525,14 +1524,6 @@ class ZooKeeper(object):
                 self.state = state
 
         self.zk.add_listener(connectionListener)
-
-        @self.zk.DataWatch(ZooKeeper.CLUSTER_STATE)
-        def watchClusterState(data, *args, **kwargs):
-            if not data:
-                LOG.warning("No cluster state available: no collections defined?")
-            else:
-                self.collections = json.loads(data.decode("utf-8"))
-                LOG.info("Updated collections: %s", self.collections)
 
         @self.zk.ChildrenWatch(ZooKeeper.LIVE_NODES_ZKNODE)
         def watchLiveNodes(children):
