61704032a2262dccf7a431eaf4b2d805ae3309ca
Jan Rydzewski <flegmer@gmail.com>
2023-11-30 14:14:45 +0100

    Remove trainling slashes from URLs

--- pysolr.py
+++ pysolr.py
@@ -484,11 +484,11 @@ class Solr(object):
 
         if len(params_encoded) < 1024:
             # Typical case.
-            path = "%s/?%s" % (handler, params_encoded)
+            path = "%s?%s" % (handler, params_encoded)
             return self._send_request("get", path)
         else:
             # Handles very long queries by submitting as a POST.
-            path = "%s/" % handler
+            path = "%s" % handler
             headers = {
                 "Content-type": "application/x-www-form-urlencoded; charset=utf-8"
             }
@@ -534,7 +534,7 @@ class Solr(object):
             path_handler = "select"
             query_vars.append("qt=%s" % safe_urlencode(handler, True))
 
-        path = "%s/" % path_handler
+        path = "%s" % path_handler
 
         if commit is None:
             commit = self.always_commit
@@ -1261,11 +1261,11 @@ class Solr(object):
 
         if len(params_encoded) < 1024:
             # Typical case.
-            path = "%s/?%s" % (handler, params_encoded)
+            path = "%s?%s" % (handler, params_encoded)
             return self._send_request("get", path)
         else:
             # Handles very long queries by submitting as a POST.
-            path = "%s/" % handler
+            path = "%s" % handler
             headers = {
                 "Content-type": "application/x-www-form-urlencoded; charset=utf-8"
             }
