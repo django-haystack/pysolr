17d83ba658daf8834e78949f3f0cf5e6f6b71e34
Dhaval Gojiya <dhavalgojiya10@gmail.com>
2025-12-12 11:06:07 +0530

    Test: Improve test suite log messages for better clarity inside run-tests.py file

--- run-tests.py
+++ run-tests.py
@@ -6,13 +6,14 @@ import unittest
 
 def main():
     try:
-        print("→ Starting unit test cases...")
-
+        print("→ Starting Solr containers...")
         subprocess.run(["./solr-docker-test-env.sh", "setup"], check=True)
+
+        print("→ Running unit test suite...")
         unittest.main(module="tests", verbosity=1)
 
     finally:
-        print("→ Unit test cases completed.")
+        print("→ Unit test suite completed.")
         subprocess.run(["./solr-docker-test-env.sh", "destroy"], check=True)
 
 
