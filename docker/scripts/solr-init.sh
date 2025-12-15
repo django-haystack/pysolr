f9580020118385d2ce00429a66c95806ded00df7
Dhaval Gojiya <dhavalgojiya10@gmail.com>
2025-12-15 12:25:19 +0530

    docs: Add README describing Solr custom schema changes for Solr 9.x.x version config

--- docker/scripts/solr-init.sh
+++ docker/scripts/solr-init.sh
@@ -5,7 +5,7 @@ echo "Solr init starting ..."
 
 echo "Preparing sitecore_configset..."
 cp -r "$TECHPRODUCTS_CONFIGSET" "$SITECORE_CONFIGSET"
-cp -r "$PYSOLR_CONFIGSET"/. "$SITECORE_CONFIGSET/conf"
+cp "$PYSOLR_CONFIGSET/solrconfig.xml" "$SITECORE_CONFIGSET/conf/solrconfig.xml"
 
 # Rename managed-schema.xml to schema.xml before uploading the configset to ZooKeeper
 mv "$SITECORE_CONFIGSET/conf/managed-schema.xml" "$SITECORE_CONFIGSET/conf/schema.xml"
@@ -19,4 +19,5 @@ curl -fsS \
     "http://solr-node0:8983/solr/admin/collections?action=CREATE&name=$SOLR_COLLECTION_NAME&numShards=1&replicationFactor=2&collection.configName=sitecore_configset&createNodeSet=solr-node0:8983_solr,solr-node1:8983_solr"
 
 echo "Collection '$SOLR_COLLECTION_NAME' created successfully."
+
 echo "Solr init completed."
